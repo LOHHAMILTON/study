934bd1f2f5cb78a10873036da6dc205680de399b
LOHHAMILTON <tttggg963@naver.com>
2022-10-24 12:43:45 +0900

    해쉬(선형 조사법)

--- Project3/Project3/소스.c
+++ Project3/Project3/소스.c
@@ -1,107 +1,99 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#define INF 99999999
-#define MAX_SIZE 1001
+#include <string.h>
+#include <time.h>
+#define TABLE_SIZE 10007
+#define INPUT_SIZE 5000
 
 typedef struct
 {
-	int index;
-	struct Node* next;
-}Node;
+	int id;
+	char name[20];
+} Student;
 
-typedef struct
+//해시 테이블을 초기화합니다.
+void init(Student** hashTable) 
 {
-	Node* front;
-	Node* rear;
-	int count;
-}Queue;
-Node** a;
-int n, m, c[MAX_SIZE];
-
-void addFront(Node* root, int index) {
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->index = index;
-	node->next = root->next;
-	root->next = node;
-}
-
-void queuePush(Queue* queue, int index)
-{
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->index = index;
-	node->next = NULL;
-	if (queue->count == 0) {
-		queue->front = node;
-	}
-	else
+	for (int i = 0; i < TABLE_SIZE; i++)
 	{
-		queue->rear->next = node;
-	}
-	{
-		queue->rear = node;
-		queue->count++;
-	}
-}
-
-int queuePop(Queue* queue)
-{
-	if (queue->count == 0)
-	{
-		printf("큐 언더플로우가 발생했습니다.\n");
-		return -INF;
-	}
-	Node* node = queue->front;
-	int index = node->index;
-	queue->front = node->next;
-	free(node);
-	queue->count--;
-	return index;
-}
-
-void bfs(int start)
-{
-	Queue q;
-	q.front = q.rear = NULL;
-	q.count = 0;
-	queuePush(&q, start);//start는 1이 초기값
-	c[start] = 1;//방문처리
-	while (q.count != 0)
-	{
-		int x = queuePop(&q);
-		printf("%d ", x);
-		Node* cur = a[x]->next;
-		while (cur != NULL)
+		if (hashTable[i] != NULL)
 		{
-			int next = cur->index;
-			if (!c[next])
-			{
-				queuePush(&q, next);
-				c[next] = 1;//방문처리
-			}
-			cur = cur->next;
+			free(hashTable[i]);
 		}
 	}
 }
 
+//해시 테이블 내 빈 공간을 선형 탐색으로 찾습니다.
+int fineEmpty(Student** hashTable, int id)
+{
+	int i = id % TABLE_SIZE;
+	while (1)
+	{
+		if (hashTable[i % TABLE_SIZE] == NULL)
+		{
+			return i % TABLE_SIZE;
+		}
+	}
+}
+//특정한 ID값에 매칭되는 데이터를 해시테이블내에서 찾습니다.
+int search(Student** hashTable, int id)
+{
+	int i = id % TABLE_SIZE;
+	while (1)
+	{
+		if (hashTable[i % TABLE_SIZE] == NULL) return -1;
+		else if (hashTable[i % TABLE_SIZE]->id == id) return i;
+		i++;
+	}
+}
+//특정한 키 인덱스에 데이터를 삽입합니다.
+void add(Student** hashTable, Student* input, int key)
+{
+	return hashTable[key];
+}
+
+//해시 테이블에서 특정한 키의 데이터를 반환합니다.
+Student* getValue(Student** hashTable, int key)
+{
+	return hashTable[key];
+}
+
+//해시테이블에 존재하는 모든데이터를 출력합니다.
+void show(Student** hashtable)
+{
+	for (int i = 0; i < TABLE_SIZE; i++)
+	{
+		if (hashtable[i] != NULL)
+		{
+			printf("키 : [&d] 이름 : [%s]\n", i, hashTable[i]->name);
+		}
+	}
+}
 int main(void)
 {
-	scanf("%d %d", &n, &m);
-	a = (Node**)malloc(sizeof(Node*) * (MAX_SIZE));
-	for (int i = 1; i <= n; i++)
+	Student** hashTable;
+	hashTable = (Student**)malloc(sizeof(Student) * TABLE_SIZE);
+	for (int i = 0; i < INPUT_SIZE; i++)
 	{
-	a[i] = (Node*)malloc(sizeof(Node));
-	a[i]->next = NULL;
+		Student* student = (Student*)malloc(sizeof(student));
+		student->id = rand() % 1000000;
+		sprintf(student->name, "사람%d", student->id);
+		if (search(hashTable, student->id) == -1) //중복되는 ID는 존재하지 않도록 함
+		{
+			int index = fineEmpty(hashTable, student->id);
+			add(hashTable, student, index);
+		}
 	}
-	for (int i = 0; i < m; i++)
-	{
-		int x, y;
-		scanf("%d %d", &x, &y);
-		addFront(a[x], y);
-		addFront(a[y], x);
-	}
-	bfs(1);
-	system("pause");
-	return 0;
+
+show(hashTable);
+destructor(hashTable);
+system("pause");
+return 0;
 
 }
+
+
+
+
+
