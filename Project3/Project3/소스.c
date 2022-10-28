adcf31e7eeaa2f57061829e7de7d844a8d8fb8b5
LOHHAMILTON <tttggg963@naver.com>
2022-10-28 12:42:03 +0900

    해쉬

--- Project3/Project3/소스.c
+++ Project3/Project3/소스.c
@@ -1,105 +1,42 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#define TABLE_SIZE 10007
-#define INPUT_SIZE 5000
+﻿#include <stdio.h>
+#define NUMBER 7
 
-typedef struct
-{
-	int id;
-	char name[1000];
-} Student;
+int tree[NUMBER];
 
-//해시 테이블을 초기화합니다.
-void init(Student** hashTable)
+int sum(int i)
 {
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		hashTable[i] = NULL;
-	}
-}
-
-void destructor(Student** hashTable)
-{
-	for (int i = 0; i < TABLE_SIZE; i++) {
-		if (hashTable[i] != NULL) {
-			free(hashTable[i]);
-		}
-	}
-}
-
-//해시 테이블 내 빈 공간을 선형 탐색으로 찾습니다.
-int fineEmpty(Student** hashTable, int id)
-{
-	int i = id % TABLE_SIZE;
-	while (1)
+	int res = 0;
+	while (i > 0)
 	{
-		if (hashTable[i % TABLE_SIZE] == NULL)
-		{
-			return i % TABLE_SIZE;
-		}
-		i++;
+		res += tree[i];
+		i -= (i & -i);
 	}
-}
-//특정한 ID값에 매칭되는 데이터를 해시테이블내에서 찾습니다.
-int search(Student** hashTable, int id)
-{
-	int i = id % TABLE_SIZE;
-	while (1)
-	{
-		if (hashTable[i % TABLE_SIZE] == NULL) return -1;
-		else if (hashTable[i % TABLE_SIZE]->id == id) return i;
-		i++;
-	}
-}
-//특정한 키 인덱스에 데이터를 삽입합니다.
-void add(Student** hashTable, Student* input, int key)
-{
-	hashTable[key] = input;
+	return res;
 }
 
-//해시 테이블에서 특정한 키의 데이터를 반환합니다.
-Student* getValue(Student** hashTable, int key)
+void update(int i, int dif)
 {
-	return hashTable[key];
-}
-
-//해시테이블에 존재하는 모든데이터를 출력합니다.
-void show(Student** hashTable)
-{
-	for (int i = 0; i < TABLE_SIZE; i++)
+	while (i <= NUMBER)
 	{
-		if (hashTable[i] != NULL)
-		{
-			printf("키 : [%d] 이름 : [%s]\n", i, hashTable[i]->name);
-		}
+		tree[i] += dif;
+		i += (i & -i);
 	}
 }
+
+int getSection(int start, int end)
+{
+	return sum(end) - sum(start - 1);
+}
+
 int main(void)
 {
-	Student** hashTable;
-	hashTable = (Student**)malloc(sizeof(Student) * TABLE_SIZE);
-	init(hashTable);
-
-	for (int i = 0; i < INPUT_SIZE; i++)
-	{
-		Student* student = (Student*)malloc(sizeof(Student));
-		student->id = rand() % 1000000;
-		sprintf(student->name, "사람%d", student->id);
-		if (search(hashTable, student->id) == -1) //중복되는 ID는 존재하지 않도록 함
-		{
-			int index = fineEmpty(hashTable, student->id);
-			add(hashTable, student, index);
-		}
-	}
-show(hashTable);
-destructor(hashTable);
-system("pause");
-return 0;
+	update(1, 7);
+	update(2, 1);
+	update(3, 9);
+	update(4, 5);
+	update(5, 6);
+	update(6, 4);
+	update(7, 1);
+	printf("1부터 7까지의 구간 합 : %d\n", getSection(1, 7));
+	system("pause");
 }
-
-
-
-
-
